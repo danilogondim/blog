d12f8aaf0a40aa6f760fbb33597980bc6bac09cb
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-13 14:25:14 +0000

    Add the articles RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  # declare a standard REST resource (then run bin/rake routes to see the created routes):
+  resources :articles
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
